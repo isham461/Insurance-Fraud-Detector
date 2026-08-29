6e3c6ab21601dd7c74145a04c5938a7ac9fc1371
isham <ishamazik@ishams-MacBook-Air.local>
2026-08-29 11:54:27 +0530

    Fix FastAPI CORS wildcard credentials bug that blocked GET requests in the browser

--- phase2_backend/main.py
+++ phase2_backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Insurance Fraud API", version="2.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
